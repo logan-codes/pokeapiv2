47230569ff3dea511799b782c1d95b75f13f0f77
Osvaldo Franco <linkkenedy@gmail.com>
2022-06-26 18:15:54 -0500

    first commit

---
+++ src/scripts/Urls.js
@@ -0,0 +1,13 @@
+const URL= {
+    PokeApiUrl : "https://pokeapi.co/api/v2/"
+}
+
+const rutas = {
+    ApiPokemons : URL.PokeApiUrl + 'pokemon',
+}
+
+const Urls = {
+    pokemons: rutas.ApiPokemons
+}
+
+export default Urls;
