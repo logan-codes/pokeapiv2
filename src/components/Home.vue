1ff0b31ae8616b32d0c171e1055a796a1252a6bd
Osvaldo Franco <linkkenedy@gmail.com>
2022-06-26 18:48:08 -0500

    pokemon

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Home">
+    
+    <p>Test</p>
+
+  </div>
+</template>
+
+<script>
+
+import Urls from '@/scripts/Urls'
+
+
+
+
+export default {
+  name: 'HomePoke',
+  data(){
+    return{
+      pokemons: [],
+    }   
+  }
+}
+
+
+
+</script>
+
+
+<style>
+
+</style>
